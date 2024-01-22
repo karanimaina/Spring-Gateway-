7c72bafb4b29ea36b0367a9d598f7d231069535f
fkarani <maina.felix@eclectics.io>
2024-01-22 09:53:56 +0300

    first commit

---
+++ src/test/java/com/example/esbgateway/EsbGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esbgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsbGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
